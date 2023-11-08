65f97392dfd5739e15bcf51700ea854f2d9bd725
Alan <lesterthenovice@gmail.com>
2023-11-08 20:59:34 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
