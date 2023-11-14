0b21207969bdcb6d761fc0ba5c01b2c93063ea85
Alan <lesterthenovice@gmail.com>
2023-11-14 20:38:59 +0000

    search functionality, authorisation

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BlogApplication.Data;
 using BlogApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BlogApplication.Controllers
 {
@@ -20,12 +21,36 @@ namespace BlogApplication.Controllers
         }
 
         // GET: Blogs
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() //Entity framework 
         {
               return _context.Blog != null ? 
                           View(await _context.Blog.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Blog'  is null.");
         }
+
+
+        // GET: Blogs/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm() //Entity framework 
+        {
+            return _context.Blog != null ?
+                        View() :
+                        Problem("Entity set 'ApplicationDbContext.Blog'  is null.");
+        }
+
+
+
+
+
+        // POST: Blogs/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase) //Entity framework 
+        {
+            return View("Index", await _context.Blog.Where(j =>j.BlogTopic.Contains(SearchPhrase)).ToListAsync()); // whole list returned
+
+
+        }
+
+
+
 
         // GET: Blogs/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -46,6 +71,8 @@ namespace BlogApplication.Controllers
         }
 
         // GET: Blogs/Create
+
+        [Microsoft.AspNetCore.Authorization.Authorize]
         public IActionResult Create()
         {
             return View();
@@ -54,6 +81,7 @@ namespace BlogApplication.Controllers
         // POST: Blogs/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,BlogTopic,BlogContent")] Blog blog)
@@ -68,6 +96,7 @@ namespace BlogApplication.Controllers
         }
 
         // GET: Blogs/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Blog == null)
@@ -82,7 +111,7 @@ namespace BlogApplication.Controllers
             }
             return View(blog);
         }
-
+        [Authorize]
         // POST: Blogs/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -117,7 +146,7 @@ namespace BlogApplication.Controllers
             }
             return View(blog);
         }
-
+        [Authorize]
         // GET: Blogs/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -135,7 +164,7 @@ namespace BlogApplication.Controllers
 
             return View(blog);
         }
-
+        [Authorize]
         // POST: Blogs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
